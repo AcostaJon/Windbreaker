6e4d46f94d0acdc8eb8a5eca30cdcf66d1b842bd
Jonathan Acosta <jonacosta86@gmail.com>
2024-12-03 13:17:27 -0500

    init

--- app/api/CurrentWeather.js
+++
@@ -1,7 +0,0 @@
-// api call for curent weather at https://www.weatherapi.com/api-explorer.aspx
-export default async function currentweather(location) {
-
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=36279875f3bd444e934214049221502&q=${location}&aqi=no`)
-        .then(res => res.json())
-    return response
-}              
