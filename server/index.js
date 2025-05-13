41faf481e5d7aa8d3f47be43fd31e6a680c248c8
Jonathan Acosta <jonacosta86@gmail.com>
2025-05-13 08:59:47 -0400

    installed client side with react and server side with express

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express();
+
+const port = 5000;
+
+// route "/"
+app.get('/', (req, res) => {
+    res.send('Hello Worlfasfdtgeqwrgt!');
+  });
+
+// listen to port
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
+
