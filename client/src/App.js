50df7d53ae52813dced770bd3ce969c4b53f913f
Jonathan Acosta <jonacosta86@gmail.com>
2025-06-18 17:19:55 -0400

    updated app.js with state and get request

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,66 @@ import axios from 'axios';
 
 function App() {
   // State
-  const [weatherData, setWeatherData] = useState("")
-  const [temp, setTemp] = useState("")
+  const [currentWeatherData, setCurrentWeatherData] = useState("")
+  const [currentAstroData, setCurrentAstroData] = useState("")
+  // current weather
+  const [currentTemp, setCurrentTemp] = useState("")
   const [conditionText, setConditionText] = useState("")
   const [conditionIcon, setConditionIcon] = useState("");
+  const [location, setLocation] = useState("")
+  const [region, setRegion] = useState("")
+  const [feelsLike, setFeelsLike] = useState("")
+  const [currentTime, setCurrentTime] = useState("")
+  // forecast box
+  const [cloudCover, setCloudCover] = useState("")
+  const [windDirection, setWindDirection] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [day1, setDay1] = useState("")
+  const [day2, setDay2] = useState("")
+  const [day3, setDay3] = useState("")
+  // astronomy
+  const [sunrise, setSunrise] = useState("")
+  const [sunset, setSunset] = useState("")
+  const [moonPhase, setMoonPhase] = useState("");
 
   // 1st: Get data
   useEffect(() => {
     fetchData()
-
   },[])
 
   async function fetchData() {
     try {
-      const response = await axios.get('https://windbreaker-server.vercel.app/get_current/02907');
+      // get current date and time
+      const now = new Date();
+      // convert to string
+      const dayAndTime = now.toString() 
+      // slice up to the year
+      setCurrentTime(dayAndTime.slice(0,15))
 
-      setWeatherData(response.data)
-      setTemp(response.data.current.temp_f)
-      setConditionText(response.data.current.condition.text)
-      setConditionIcon(response.data.current.condition.icon)
+      // GET request 
+      const getCurrent = await axios.get('https://windbreaker-server.vercel.app/get_current/02907');
+      const getAstronomy = await axios.get('https://windbreaker-server.vercel.app/get_astronomy/02907')
+      // update state
+      // current
+      setCurrentWeatherData(getCurrent.data);
+      setCurrentAstroData(getAstronomy.data)
+      setCurrentTemp(getCurrent.data.current.temp_f);
+      setConditionText(getCurrent.data.current.condition.text);
+      setConditionIcon(getCurrent.data.current.condition.icon);
+      setLocation(getCurrent.data.location.name);
+      setRegion(getCurrent.data.location.region);
+      setFeelsLike(getCurrent.data.current.feelslike_f);
+      // forecast
+      setCloudCover(getCurrent.data.current.cloud);
+      setWindDirection(getCurrent.data.current.wind_dir);
+      setHumidity(getCurrent.data.current.humidity);
+      setDay1(getCurrent.data.forecast.forecastday[0]);
+      setDay2(getCurrent.data.forecast.forecastday[1]);
+      setDay3(getCurrent.data.forecast.forecastday[2]);
+      // astronomy
+      setSunrise(getAstronomy.data.astronomy.astro.sunrise);
+      setSunset(getAstronomy.data.astronomy.astro.sunset);
+      setMoonPhase(getAstronomy.data.astronomy.astro.moon_phase);
 
     } catch (error) {
       console.error(error);
