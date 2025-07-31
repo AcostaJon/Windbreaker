57271be8940d24bae18bb72bb0e8cebad5b46434
Jonathan Acosta <jonacosta86@gmail.com>
2025-07-30 21:08:48 -0400

    ui and style update

---
+++ client/src/components/storedLocation/storedLocation.js
@@ -0,0 +1,17 @@
+export default function StoredLocation(params) {
+    return (
+        <div id="storedLocation">
+            {/* content Container */}
+            <div>
+                {/* current temp */}
+                <p></p>
+                {/* location */}
+                <p></p>
+                {/* condition image */}
+                <img />
+            </div>
+            {/* remove button */}
+            <button>X</button>
+        </div>
+    )
+}
